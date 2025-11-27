920cef0709b2f94070e8a5249b7f991c94e022ee
User <ulyanovskiy.2001@mail.ru>
2025-11-27 14:32:02 +0300

    feat: configure basic database context

--- Npgsql.EFCore.Tracker.Api.Demo/Program.cs
+++ Npgsql.EFCore.Tracker.Api.Demo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EFCore.Tracker.Api.Demo.Database;
 using Npgsql.EFCore.Tracker.AspNet.Extensions;
 using System.Reflection;
 
@@ -6,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddControllers();
 
     builder.Services.AddOpenApi();
+
+    builder.Services.AddDbContext<DatabaseContext>(options =>
+    {
+        options.UseNpgsql("Host=localhost;Port=5432;Database=main;Username=postgres;Password=postgres");
+    });
 
     builder.Services.AddTrackerSupport(Assembly.GetExecutingAssembly());
 }
