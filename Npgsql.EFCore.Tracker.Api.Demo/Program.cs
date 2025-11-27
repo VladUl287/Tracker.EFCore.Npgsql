88a732b2ec278b8e1b855d46ef0cec28313946c2
User <ulyanovskiy.2001@mail.ru>
2025-11-27 13:45:01 +0300

    fix: remove unused code and comments

--- Npgsql.EFCore.Tracker.Api.Demo/Program.cs
+++ Npgsql.EFCore.Tracker.Api.Demo/Program.cs
@@ -1,22 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using Npgsql.EFCore.Tracker.Api.Demo.Database;
-using Npgsql.EFCore.Tracker.Core.Extensions;
-using Npgsql.EFCore.Tracker.AspNet.Extensions;
-using Npgsql.EFCore.Tracker.AspNet.Middlewares;
-
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
-
-    //builder.Services.AddDbContext<DatabaseContext>(options =>
-    //{
-    //    options.UseNpgsql(
-    //        "Host=localhost;Port=5432;Database=HubDb;Username=postgres;Password=qwerty",
-    //        npgsql =>
-    //        {
-    //            npgsql.EnableTableTrackingSupport();
-    //        });
-    //});
 
     builder.Services.AddOpenApi();
 
@@ -24,8 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
-    app.UseChangeTracker();
-
     if (app.Environment.IsDevelopment())
     {
         app.MapOpenApi();
