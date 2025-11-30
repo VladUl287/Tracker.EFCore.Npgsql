8447ddc381915957c2a0ae5df9540f4e342c0c04
User <ulyanovskiy.2001@mail.ru>
2025-11-30 18:59:45 +0300

    feat: create route endpoint route resolver contract

---
+++ Tracker.AspNet/Services/Contracts/IEndpointRouteResolver.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Tracker.AspNet.Services.Contracts;
+
+public interface IEndpointRouteResolver
+{
+    string ResolveRoute(RouteEndpoint endpoint);
+}
