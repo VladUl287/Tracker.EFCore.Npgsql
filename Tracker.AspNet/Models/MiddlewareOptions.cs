1c7129deb3a9d5b97a41b9aea9aad07b991b61e3
User <ulyanovskiy.2001@mail.ru>
2025-12-01 17:43:35 +0300

    feat: add suffix to options for etag generation

--- Tracker.AspNet/Models/MiddlewareOptions.cs
+++ Tracker.AspNet/Models/MiddlewareOptions.cs
@@ -5,6 +5,9 @@ namespace Tracker.AspNet.Models;
 public sealed class MiddlewareOptions
 {
     public Func<HttpContext, bool> Filter { get; set; } = (_) => true;
+    
     public string[] Tables { get; set; } = [];
     public Type[] Entities { get; set; } = [];
+
+    public Func<HttpContext, string> Suffix { get; set; } = (_) => string.Empty;
 }
