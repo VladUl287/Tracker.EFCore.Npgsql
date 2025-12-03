104c7d2f071496c1835971275625c645decee280
User <ulyanovskiy.2001@mail.ru>
2025-12-03 16:41:45 +0300

    feat: create basic dboperations service

---
+++ Tracker.AspNet/Services/Contracts/IDbOperations.cs
@@ -0,0 +1,7 @@
+﻿namespace Tracker.AspNet.Services.Contracts;
+
+public interface IDbOperations
+{
+    Task<DateTimeOffset?> GetLastTimestamp(string table);
+    Task<uint?> GetLastCommittedXact(string table);
+}
