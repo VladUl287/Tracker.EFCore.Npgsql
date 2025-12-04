05c7f0d9cb53e0f69a4c1abb4442fb1c6aae0a10
User <ulyanovskiy.2001@mail.ru>
2025-12-04 22:35:42 +0300

    feat: add first ISourceOperations property for fast access for not presented sourceId

--- Tracker.AspNet/Services/SourceOperationsResolver.cs
+++ Tracker.AspNet/Services/SourceOperationsResolver.cs
@@ -3,17 +3,19 @@ using Tracker.AspNet.Services.Contracts;
 
 namespace Tracker.AspNet.Services;
 
-public sealed class SourceOperationsResolver(
-    IEnumerable<ISourceOperations> sourceOperations) : ISourceOperationsResolver
+public sealed class SourceOperationsResolver(IEnumerable<ISourceOperations> sourceOperations) : ISourceOperationsResolver
 {
-    private readonly FrozenDictionary<string, ISourceOperations> _store
-        = sourceOperations.ToFrozenDictionary(c => c.SourceId);
+    private readonly FrozenDictionary<string, ISourceOperations> _store = 
+        sourceOperations.ToFrozenDictionary(c => c.SourceId);
+
+    private readonly ISourceOperations _first = 
+        sourceOperations.First();
 
     public ISourceOperations Resolve(string? sourceId)
     {
         if (sourceId is not null && _store.TryGetValue(sourceId, out var value))
             return value;
 
-        return _store.Values[0];
+        return _first;
     }
 }
