6ab6bf23a67363145371ad43e32146cadb4838d2
User <ulyanovskiy.2001@mail.ru>
2025-12-04 22:20:39 +0300

    feat: add not ef core specific minimal api registrations

--- Tracker.AspNet/Extensions/EndpointBuilderExtensions.cs
+++ Tracker.AspNet/Extensions/EndpointBuilderExtensions.cs
@@ -31,11 +31,33 @@ public static class EndpointBuilderExtensions
         });
     }
 
+    public static IEndpointConventionBuilder WithTracking(this IEndpointConventionBuilder endpoint, GlobalOptions options)
+    {
+        return endpoint.AddEndpointFilterFactory((provider, next) =>
+        {
+            var builder = provider.ApplicationServices.GetRequiredService<IOptionsBuilder<GlobalOptions, ImmutableGlobalOptions>>();
+            var immutableOptions = builder.Build(options);
+
+            var etagService = provider.ApplicationServices.GetRequiredService<IETagService>();
+            var requestFilter = provider.ApplicationServices.GetRequiredService<IRequestFilter>();
+
+            var filter = new TrackerEndpointFilter(etagService, requestFilter, immutableOptions);
+            return (context) => filter.InvokeAsync(context, next);
+        });
+    }
+
     public static IEndpointConventionBuilder WithTracking<TContext>(this IEndpointConventionBuilder endpoint, Action<GlobalOptions> configure)
         where TContext : DbContext
     {
         var options = new GlobalOptions();
         configure(options);
         return endpoint.WithTracking<TContext>(options);
+    }
+
+    public static IEndpointConventionBuilder WithTracking(this IEndpointConventionBuilder endpoint, Action<GlobalOptions> configure)
+    {
+        var options = new GlobalOptions();
+        configure(options);
+        return endpoint.WithTracking(options);
     }
 }
