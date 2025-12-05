e85f44e49b8e8dd25f23bd732f5354c1cc850377
User <ulyanovskiy.2001@mail.ru>
2025-12-05 20:47:09 +0300

    feat: init cache control builder

---
+++ Tracker.AspNet/Utils/CacheControlBuilder.cs
@@ -0,0 +1,17 @@
+﻿namespace Tracker.AspNet.Utils;
+
+public sealed class CacheControlBuilder
+{
+    private readonly List<string> _directives;
+
+    public CacheControlBuilder() => _directives = [];
+    public CacheControlBuilder(int capacitiy) => _directives = new(capacitiy);
+
+    public CacheControlBuilder WithDirective(string directive)
+    {
+        _directives.Add(directive);
+        return this;
+    }
+
+    public string Build() => $"Cache-Control: {string.Join(',', _directives)}";
+}
