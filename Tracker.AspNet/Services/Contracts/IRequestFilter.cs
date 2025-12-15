99ad5d47660f76c64dadafa9f5225af1b6f9e1a0
User <ulyanovskiy.2001@mail.ru>
2025-12-15 10:25:00 +0300

    docs: request fitler xml documenation

--- Tracker.AspNet/Services/Contracts/IRequestFilter.cs
+++ Tracker.AspNet/Services/Contracts/IRequestFilter.cs
@@ -3,7 +3,20 @@ using Tracker.AspNet.Models;
 
 namespace Tracker.AspNet.Services.Contracts;
 
+/// <summary>
+/// Determines whether ETag generation and comparison are permitted based on context data, 
+/// configured options, and HTTP specification compliance requirements.
+/// </summary>
 public interface IRequestFilter
 {
+    /// <summary>
+    /// Determines if the HTTP request qualifies for ETag operations according to
+    /// protocol rules and application configuration.    
+    /// </summary>
+    /// <param name="context">The HTTP context containing request and response details to evaluate.</param>
+    /// <param name="options">The immutable global configuration options defining ETag behavior rules.</param>
+    /// <returns>
+    /// <c>true</c> if the provided context matches the expected validation rules, otherwise, <c>false</c>.
+    /// </returns>
     bool RequestValid(HttpContext context, ImmutableGlobalOptions options);
 }
--- Tracker.AspNet/Services/DefaultRequestFilter.cs
+++ Tracker.AspNet/Services/DefaultRequestFilter.cs
@@ -9,6 +9,10 @@ using Tracker.AspNet.Services.Contracts;
 
 namespace Tracker.AspNet.Services;
 
+/// <summary>
+/// Basic implementation of <see cref="IRequestFilter"/> which determines whether ETag generation and comparison are permitted based on context data, 
+/// configured options, and HTTP specification compliance requirements.
+/// </summary>
 public sealed class DefaultRequestFilter(ILogger<DefaultRequestFilter> logger) : IRequestFilter
 {
     private static readonly string[] _invalidRequestDirectives = ["no-transform", "no-store"];
