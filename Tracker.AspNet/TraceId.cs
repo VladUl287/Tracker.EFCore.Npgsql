fe1093cbdcec33c6c3b055ea71a5484a1705a93a
User <ulyanovskiy.2001@mail.ru>
2025-12-18 18:28:12 +0300

    docs: traceid xml comments

--- Tracker.AspNet/TraceId.cs
+++ Tracker.AspNet/TraceId.cs
@@ -2,6 +2,10 @@
 
 namespace Tracker.AspNet;
 
+/// <summary>
+/// Wraps <see cref="HttpContext.TraceIdentifier"/> to defer string allocation until needed.
+/// Used when logging is disabled to avoid unnecessary string creation.
+/// </summary>
 public readonly struct TraceId(HttpContext httpContext)
 {
     public override string ToString() => httpContext.TraceIdentifier;
