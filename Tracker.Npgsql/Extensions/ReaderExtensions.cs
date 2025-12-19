97b311d35a97d938015ae2ec93d5820b43255656
User <ulyanovskiy.2001@mail.ru>
2025-12-19 11:08:21 +0300

    docs: reader extensions xml comment with link to resource

--- Tracker.Npgsql/Extensions/ReaderExtensions.cs
+++ Tracker.Npgsql/Extensions/ReaderExtensions.cs
@@ -9,6 +9,12 @@ internal static class ReaderExtensions
 {
     private const long PostgresTimestampOffsetTicks = 630822816000000000L;
 
+    /// <summary>
+    /// Adapted from <a href="https://github.com/npgsql/npgsql/blob/main/src/Npgsql/Internal/Converters/Temporal/PgTimestamp.cs">source documentation</a>
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="ordinal"></param>
+    /// <returns></returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     internal static long GetTimestampTicks(this NpgsqlDataReader reader, int ordinal) =>
         reader.GetInt64(ordinal) * 10 + PostgresTimestampOffsetTicks;
